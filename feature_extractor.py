6c66a290e166bd64d670dc07f4c855c3fe96fae1
emils <emilsvob@gmail.com>
2026-04-02 21:19:05 +0200

    clean weight loading

--- feature_extractor.py
+++ feature_extractor.py
@@ -2,59 +2,67 @@ import requests
 import os
 
 import timm
+import timm.data
 import torch
 
 from typing import Literal
 
-from timm.models import load_pretrained
+from timm.models.eva import checkpoint_filter_fn
+
 
 class EsophagitisFeatureExtractor(torch.nn.Module):
     def __init__(self,
                  size: Literal["small", "base", "large"],
                  model_dir_path="./SSL-Esophagitis-Detection/downloaded_models",
                  pretrained_on_esophagitis=True,
-                 use_qkvb = True,
                  use_global_pool = True,
+                 use_strict_weight_loading = True,
+                 use_native_transform = True,
                  *args, **kwargs):
         super().__init__()
 
         self.vision_model_size = size
-        self.vision_model_name = f'vit_{self.vision_model_size}_patch16_dinov3'
+        self.use_native_transform = use_native_transform
+        self.vision_model_dict = {
+            "small": "vit_small_patch16_dinov3_qkvb.lvd1689m",
+            "base": "vit_base_patch16_dinov3_qkvb.lvd1689m",
+            "large": "vit_large_patch16_dinov3_qkvb.lvd1689m"
+        }
+
+        self.vision_model_name = self.vision_model_dict[size]
         self.pretrained_on_esophagitis = pretrained_on_esophagitis
 
-        if use_qkvb:
-            self.vision_model_name = self.vision_model_name + "_qkvb"
+        # if use_qkvb:
+        #     self.vision_model_name = self.vision_model_name + "_qkvb"
 
         self.model_dir_path = model_dir_path
         self.model_path = f"{model_dir_path}/dinov3-vit{self.vision_model_size[0]}16-esophagitis-detector.pth"
 
-        self.vision_model = timm.create_model(self.vision_model_name, pretrained=True, features_only=True)
+        self.vision_model = timm.create_model(self.vision_model_name,
+                                              pretrained=True,
+                                              features_only=True,
+                                              pretrained_cfg_overlay={
+                                                  "file": self.model_path if pretrained_on_esophagitis else None,
+                                              }
+                                              )
+
         self.global_pool = torch.nn.AdaptiveAvgPool2d((1, 1)) if use_global_pool else None
 
+        self.use_strict_weight_loading = use_strict_weight_loading
 
-
-        if self.pretrained_on_esophagitis:
-            if not os.path.exists(self.model_path):
-                print("Model not found locally. Downloading...")
-                self.download_model()
-
-            print(f"Loading EsophagitisFeatureExtractor model from {self.model_path}...")
-
-            try:
-                ckpt = torch.load(self.model_path, weights_only=False)
-
-                # The state dict keys in the checkpoint are expected to be prefixed with "model.",
-                # so we add that prefix to each key before loading.
-                state_dict = {"model."+k : v for k, v in ckpt.items()}
-
-                self.vision_model.load_state_dict(state_dict, strict=False)
-
-            except Exception as e:
-                raise Exception("Error loading state dict with strict=True. Reason:", e)
+        data_config = timm.data.resolve_model_data_config(self.vision_model)
+        self.transform = timm.data.create_transform(**data_config,
+                                                    is_training=False)
 
     def forward(self, x,
                 *args, **kwargs):
-        output = self.vision_model(x)
+
+        if self.use_native_transform:
+            transformed_output = self.transform(x)
+        else:
+            transformed_output = x
+
+        output = self.vision_model(transformed_output)
 
         if self.global_pool is not None:
             output = self.global_pool(output[-1])  # Apply global pooling to the last feature map
